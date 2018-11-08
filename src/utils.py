8004326e48f7d56b50c9492b980247de57b55574
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-11-07 20:42:28 -0600

    update helper functions

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ Helper Functions.
 import os
 import json
 import shutil
+import logging
 
 import torch
 
@@ -46,6 +47,48 @@ class Params():
     def dict(self):
         """Give dict-like access to Params instance by `params.dict['learning_rate']."""
         return self.__dict__
+
+
+def set_logger(log_path):
+    """
+    Set the logger to log info in terminal and file `log_path`.
+
+    In general, it is useful to have a logger so that every output to the terminal is saved
+    in a permanent file. Here we save it to `model_dir/train.log`.
+    Example:
+    ```
+    logging.info("Starting training...")
+    ```
+    Args:
+        log_path: (string) where to log
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        logger.addHandler(file_handler)
+
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
+
+
+def save_dict_to_json(d, json_path):
+    """
+    Save dict of floats in json file.
+
+    Args:
+        d: (dict) of float-castable values (np.float, int, float, etc.)
+        json_path: (string) path to json file
+    """
+    with open(json_path, 'w') as f:
+        # We need to convert the values to float for json (it doesn't accept np.array, np.float, )
+        d = {k: float(v) for k, v in d.items()}
+        json.dump(d, f, indent=4)
 
 
 def save_checkpoint(state, is_best, checkpoint):
@@ -90,6 +133,3 @@ def load_checkpoint(checkpoint, model, optimizer=None):
         optimizer.load_state_dict(checkpoint['optim_dict'])
 
     return checkpoint
-
-
-#
