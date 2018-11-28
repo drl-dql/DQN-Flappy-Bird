6280143979c900ffe826fc38b47774b2570958cd
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-11-28 12:06:18 -0600

    update bw sh utils

--- utils/gen_pbs.py
+++ utils/gen_pbs.py
@@ -1,11 +1,11 @@
 """This script creates a PBS file that runs one hyperameter setting."""
 
 
-training_file = "main.py"
+training_file = "main.py --train=True --video_path=./video --logs_path=./logs"
 walltime = "48:00:00"
-jobname = "drl-dql"
+jobname = "ddqn-flappybird"
 netid = "netid"
-directory = "~/scratch/drl-dql/"
+directory = "~/scratch/ddqn-flappybird/"
 
 print "#!/bin/bash"
 print "#PBS -l nodes=1:ppn=16:xk"
@@ -20,8 +20,10 @@ print "#PBS -M {0}@illinois.edu".format(netid)
 print "cd {0}".format(directory)
 
 print ". /opt/modules/default/init/bash"
-print "module load bwpy/2.0.0-pre2"
+print "module load bwpy/2.0.0-pre4"
+print "module load gcc/5.3.0"
 print "module load cudatoolkit"
+print "source test2/bin/activate"
 
 # Change this if your hyperameters change
 print "aprun -n 1 -N 1 python {0}".format(training_file)
