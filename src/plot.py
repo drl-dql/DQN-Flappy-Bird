6678c61b83a0bc1ea9b0202d1d5d6fba13af8d8b
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-11-27 18:14:52 -0600

    add plot codes

---
+++ src/plot.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+import numpy as np
+from visdom import Visdom
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--logs_path', dest='logs_path',
+                        help='path of the checkpoint folder', default='./logs', type=str)
+    args = parser.parse_args()
+
+    return args
+
+
+args = parse_args()
+
+
+def main():
+    logs_path = args.logs_path
+    episode, reward = zip(*np.load(os.path.join(logs_path, 'reward.npy')))
+    _, loss = zip(*np.load(os.path.join(logs_path, 'loss.npy')))
+
+    avg_reward = np.cumsum(reward) / np.arange(1, len(reward) + 1)
+
+    print(np.array(episode).reshape(-1, 1).repeat(3, 1).shape)
+    print(np.column_stack([reward, avg_reward, loss]).shape)
+
+    viz = Visdom(env='main')
+    viz.line(
+        X=np.array(episode).reshape(-1, 1).repeat(3, 1),
+        Y=np.column_stack([reward, avg_reward, loss]),
+        opts=dict(
+            legend=['Reward', 'Average Reward', 'Loss']
+        )
+    )
+
+
+if __name__ == '__main__':
+    main()
